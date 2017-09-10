13104f28560a0994add1540d418134b0a867c63e
Marinescu Evghenii <marinescuEvghenii@gmail.com>
2017-09-11 01:52:19 +0200

    First commit

---
+++ MMM-Cursor.js
@@ -0,0 +1,67 @@
+/* global Module */
+
+/* Magic Mirror
+ * Module: MMM-Cursor
+ *
+ * By Evghenii Marinescu https://github.com/MarinescuEvghenii/
+ * MIT Licensed.
+ */
+
+Module.register("MMM-Cursor", {
+
+	defaults: {
+		timeout: 1000
+	},
+
+	getStyles: function() {
+		return ["MMM-Cursor.css"];
+	},
+
+	start: function() {
+		this.config = Object.assign({}, this.defaults, this.config);
+
+		this.bind();
+		this.hideCursor();
+
+		Log.info("Starting module: " + this.name);
+	},
+
+	bind: function() {
+		var self = this;
+
+		document.addEventListener("mousemove", function() {
+			self.onMouseMove();
+		});
+	},
+
+	hideCursor: function() {
+		var el = document.querySelector('html');
+		el.classList.add("inactive");
+
+		this.cursorVisible = false;
+	},
+
+	showCursor: function() {
+		var el = document.querySelector('html');
+		el.classList.remove("inactive");
+
+		this.cursorVisible = true;
+	},
+
+	onMouseMove: function() {
+		var self = this;
+
+		if(self.timer) {
+			clearTimeout(self.timer);
+			delete self.timer;
+		}
+
+		if(!self.cursorVisible) {
+			self.showCursor();
+		}
+
+		self.timer = setTimeout(function() {
+			self.hideCursor();
+		}, self.defaults.timeout);
+	}
+});
